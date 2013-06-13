fe2daccb5852cd5555d1dcf534c237adc181f6b1
landart <correo@jorgealbaladejo.com>
2013-06-13 16:21:55 +0200

    never, ever, extend a JS primitive object

--- js/helper.js
+++ js/helper.js
@@ -48,27 +48,14 @@ function getRandomName(){
   return options[Math.floor(Math.random()*options.length)];
 }
 
-
-Array.prototype.compare = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
-
-    // compare lengths - can save a lot of time
-    if (this.length != array.length)
-        return false;
-
-    for (var i = 0; i < this.length; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].compare(array[i]))
-                return false;
-        }
-        else if (this[i] != array[i]) {
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;
-        }
+$.fn.equals = function(compareTo) {
+  if (!compareTo || this.length != compareTo.length) {
+    return false;
+  }
+  for (var i = 0; i < this.length; ++i) {
+    if (this[i] !== compareTo[i]) {
+      return false;
     }
-    return true;
-}
+  }
+  return true;
+};
